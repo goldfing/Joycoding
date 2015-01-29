85e2fc4c5b490f9a5b4dce7e59ec10a570f9b518
한스(이한준) <goldfing@coupang.com>
2015-01-29 16:34:53 +0900

    'CSharp'

---
+++ CSharp/brainCSharp5.0/chapter02/Exercise02-01/Exercise02-01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("여러분, 안녕하세요?");
+            Console.WriteLine("반갑습니다!");
+        }
+    }
+}
---
+++ CSharp/brainCSharp5.0/chapter06/SwapByRef/SwapByRef/MainApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapByRef
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+
+            Console.WriteLine("x:{0}, y:{1}", x, y);
+
+            Swap(ref x, ref y);
+
+            Console.WriteLine("x:{0}, y:{1}", x, y);
+
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = b;
+            b = a;
+            a = temp;
+        }
+    }
+}
